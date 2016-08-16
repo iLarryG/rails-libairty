aa19bfc4bb16a54af8132bf2e95416ae8a260529
Larry Grutman <larry.grutman@gmail.com>
2016-08-16 11:34:13 +0200

    add facebook login models and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :places, only: [:index, :show] do
+    # resources :users, only: [:new, :create]
+    resources :bookings, only: [:show, :new] 
+  end
+
+  resource :profile
+
+  namespace :profile do
+    resources :places, only: [:new, :index]
+    resources :bookings, only: [:index]
+  end
 end
+
+# /places/ index
+# /places/:id show
+# /places/:id/bookings/new
+# /places/:id/bookings/:id
+# /profile/places/new
+# /profile
+# /profile/places/
